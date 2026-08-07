ad0dacaaf06863e912ab3fb7d729dd9f55824581
Herón Molina <heron.molina@gmail.com>
2026-08-07 12:25:29 -0600

    RDV completo: rutas públicas arregladas, marca de agua del logo, áreas del negocio y build contra el backend nuevo (rdv-pos@799cc6c)

--- assets/index-CnCJLWCU.js
+++ assets/index-CnCJLWCU.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-BLtVJqJn.js";/*! *****************************************************************************
+import{g as Xu}from"./index-PTZLHW5F.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
