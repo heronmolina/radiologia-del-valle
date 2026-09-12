d5a17547f1a6068505905df54ce6203073bda530
Herón Molina <heron.molina@gmail.com>
2026-09-11 20:08:11 -0600

    Emblema real en el encabezado y marca de agua 50% más grande

--- assets/index-DkPwLwUf.js
+++ assets/index-DkPwLwUf.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-PTZLHW5F.js";/*! *****************************************************************************
+import{g as Xu}from"./index-CGjsXB8G.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
